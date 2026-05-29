c902ff781cd4757c05e6a48782addfcdd57f4417
zealotwithcharge <chwang22@cmc.edu>
2026-05-29 13:49:18 -0400

    increased font size

--- card.gd
+++ card.gd
@@ -6,7 +6,8 @@ var card_data
 func setup(card, selected):
 	card_data = card
 	self.text = card["letter"]
-
+	
+	add_theme_font_size_override("font_size", 48)
 	if selected:
 		self.modulate = Color(0.7, 1.0, 0.7)
 	else:
